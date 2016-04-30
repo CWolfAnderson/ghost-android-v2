e4e774602fabab7ec75fe350e7eb1e423a017602
Christoph Anderson <cwolfanderson@gmail.com>
2016-04-30 10:55:17 -0700

    Fix edge cases even more

--- app/src/main/java/com/google/engedu/ghost/TrieNode.java
+++ app/src/main/java/com/google/engedu/ghost/TrieNode.java
@@ -128,50 +128,31 @@ public class TrieNode {
         random = new Random();
         int randomIndex;
         ArrayList<String> children;
-        // letters where isWord is false
         ArrayList<String> potentialChildren;
-        ArrayList<String> notPotentialChildren;
         String randomLetter;
 
         while (true) {
 
-            children = new ArrayList<>(current.children.keySet()); // e
+            children = new ArrayList<>(current.children.keySet()); // r
             potentialChildren = new ArrayList<>();
-            notPotentialChildren = new ArrayList<>();
 
             for (String letter : children) {
                 if (!current.children.get(letter).isWord) {
-                    Log.d("test", "this cannot form a word: " + current.children.get(letter));
                     potentialChildren.add(letter);
-                } else {
-                    notPotentialChildren.add(letter);
                 }
             }
 
             if (potentialChildren.isEmpty()) {
-                Log.d("test", "potentialChildren is empty");
-//                randomIndex = random.nextInt(children.size());
-//                randomLetter = children.get(randomIndex);
+                //TODO: make sure randomLetter.isWord is not true
                 char letter = (char) (random.nextInt(26) + 'a');
                 randomLetter = Character.toString(letter);
-//                return Character.toString(letter);
             } else {
                 randomIndex = random.nextInt(potentialChildren.size());
                 randomLetter = potentialChildren.get(randomIndex);
-//                word.append(randomLetter);
-                Log.d("test", "returning " + word);
             }
 
-           //  word.append(randomLetter);
             word.append(randomLetter);
-            current = current.children.get(randomLetter);
             return word.toString();
-//            word.append(randomLetter);
-
-//            if (word.length() > 3 && !current.isWord) {
-//            if (word.length() > 3) {
-//                return word.toString();
-//            }
 
         }
 
